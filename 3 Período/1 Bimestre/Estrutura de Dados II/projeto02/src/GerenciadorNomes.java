4f8b273a4766ef6705a5c4a3889ec74b0423397f
thalisonsantos1 <thalisonsantos.adm@outlook.com.br>
2025-04-23 17:18:42 -0300

    inicio projeto2 est dados

---
+++ 3 Período/1 Bimestre/Estrutura de Dados II/projeto02/src/GerenciadorNomes.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GerenciadorNomes {
+    private ArrayList<String> nomes;
+
+        public GerenciadorNomes() {
+            this.nomes = new ArrayList<>();
+        }
+
+        //método para adiciocar nomes se for válido
+        public boolean adicionaNome(String nome){
+            if (nome == null || nome.trim().isEmpty()){
+                System.out.println("Erro: nome não pode ser vazio!!");
+                return false;
+            }
+            if (nomes.contains(nome)){
+                System.out.println("Erro: o nome informado já existe na Lista!!");
+                return false;
+            }
+            nomes.add(nome);
+            return true;
+        }
+
+
+        // método para realizar a remoção de um nome existente
+        public boolean removeNome(String nome){
+            return nomes.remove(nome);
+        }
+
+        // buscando um nome existente na lista
+        public boolean buscaNome(String nome){
+            return nomes.contains(nome);
+        }
+
+        // Retornando a lista ordenada
+        public ArrayList<String> nomesOrdenados(){
+            ArrayList<String> copia = new ArrayList<>(nomes);
+            Collections.sort(copia);
+            return copia;
+        }
+
+        public ArrayList<String> getNomes(){
+            return nomes;
+        }
+
+        public void setNomes(ArrayList<String>nomes){
+            if (nomes != null) {
+                this.nomes = nomes;
+            } else {
+                this.nomes = new ArrayList<>();
+            }
+        }   
+        
+}
+    
+
