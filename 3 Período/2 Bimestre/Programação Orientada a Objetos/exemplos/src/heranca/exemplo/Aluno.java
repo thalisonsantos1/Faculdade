0976a4ffe869b4dab66bed7b2f8e77d5d19e0fcb
Thalison Santos <135137450+thalisonsantos1@users.noreply.github.com>
2025-04-23 21:48:03 -0300

    aula POO 23-04-25

---
+++ 3 Período/2 Bimestre/Programação Orientada a Objetos/exemplos/src/heranca/exemplo/Aluno.java
@@ -0,0 +1,14 @@
+package heranca.exemplo;
+
+public class Aluno extends Pessoa {
+    private double nota;
+    
+    public double getNota() {
+        return nota;
+    }
+    
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+}
---
+++ 3 Período/2 Bimestre/Programação Orientada a Objetos/exemplos/src/heranca/exemplo/Main.java
@@ -0,0 +1,10 @@
+package heranca.exemplo;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno a = new Aluno();
+        a.setNota(90.0);
+        a.setNome("Joaquim");
+        a.setCpf("44196283837");
+    }
+}
