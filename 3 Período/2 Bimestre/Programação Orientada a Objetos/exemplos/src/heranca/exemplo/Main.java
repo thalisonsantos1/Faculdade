ba484795848720366e3de9a4f6eb091cf970b176
Thalison Santos <135137450+thalisonsantos1@users.noreply.github.com>
2025-04-23 21:50:34 -0300

    att aula POO 23-04-25

--- 3 Período/2 Bimestre/Programação Orientada a Objetos/exemplos/src/heranca/exemplo/Main.java
+++ 3 Período/2 Bimestre/Programação Orientada a Objetos/exemplos/src/heranca/exemplo/Main.java
@@ -6,5 +6,12 @@ public class Main {
         a.setNota(90.0);
         a.setNome("Joaquim");
         a.setCpf("44196283837");
+
+// crie uma classe professor que herda de Pessoa e tem como atributo adicional o valor de salário
+
+        Professor p = new Professor();
+        p.setSalario(1000.0);
+        p.setNome("Joaquim");
+        p.setCpf("44196283837");
     }
 }
---
+++ 3 Período/2 Bimestre/Programação Orientada a Objetos/exemplos/src/heranca/exemplo/Professor.java
@@ -0,0 +1,13 @@
+package heranca.exemplo;
+
+public class Professor extends Pessoa {
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {    
+        this.salario = salario;
+    }
+}
