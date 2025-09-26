c34516532a2d310ec6ff640d5bbb01cb1d007e27
Thalison Santos <135137450+thalisonsantos1@users.noreply.github.com>
2025-09-25 21:51:36 -0300

    att

--- 4 Periodo/Desenovolvimento Sistemas Web I/004-projeto/app/db/session.py
+++ 4 Periodo/Desenovolvimento Sistemas Web I/004-projeto/app/db/session.py
@@ -1,5 +1,5 @@
 # definir a conexao com o banco e a sessao (porta de entrada para executar consultas)
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
@@ -7,5 +7,11 @@ engine = create_engine(
     settings.DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 )
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
