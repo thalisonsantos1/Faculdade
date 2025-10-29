bd83072e024c48755004ca2935eff38cac542460
Thalison Santos <135137450+thalisonsantos1@users.noreply.github.com>
2025-10-29 20:03:49 -0300

    att

--- 4 Periodo/Gestão de Banco de Dados/2 Bim/app_editora.py
+++ 4 Periodo/Gestão de Banco de Dados/2 Bim/app_editora.py
@@ -64,11 +64,11 @@ def atualizar_editora (id_editora, novo_nome):
         return
     try:
         with conn.cursor() as cursor:
-            #SQL Parametrização para evitar SQL Injection
-        sql = "UPDATE tbl_editora SET nomeeditora = %s WHERE ideditora = %s where ideditora = %s"
-        #a ordem dos parametros da tupla (novo_nome, id_editora) deve bater com a ordem %s no sql
-        cursor.execute(sql, (novo_nome, id_editora, id_editora))
-        # cursor.rowcount diz quantas linhas foram afetadas pela atualização
+            #SQL Parametrização para evitar SQL Injection        
+            sql = "UPDATE tbl_editora SET nomeeditora = %s WHERE ideditora = %s RETURNING ideditora"
+            #a ordem dos parametros da tupla (novo_nome, id_editora) deve bater com a ordem %s no sql
+            cursor.execute(sql, (novo_nome, id_editora))
+            # cursor.rowcount diz quantas linhas foram afetadas pela atualização
         if cursor.rowcount == 0:
             print (f"Nenhuma editora encontrada com o ID {id_editora}. Nada foi atualizado")
         else:
@@ -105,7 +105,67 @@ def excluir_editora(id_editora):
 
         # explicação didática importante
 
-        print ("Nota: Se esta ediroa estiver sendo usada por um livro na "tbl_livro", o PostgreSQL vai impedir a exclusão (erro de chave estrangeira). Mesmo que seu SQL tenha 'On delete cascade' (isso aplica se aplica a tbl_livro), nao a tbl_editora sendo referenciada")
+        #print ('"Nota: Se esta ediroa estiver sendo usada por um livro na "tbl_livro", o PostgreSQL vai impedir a exclusão (erro de chave estrangeira). Mesmo que seu SQL tenha 'On delete cascade' (isso aplica se aplica a tbl_livro), nao a tbl_editora sendo referenciada"')
 
     finally:
         conn.close
+
+
+# ----------Menu Principal
+
+
+def menu ():
+    """Exibe o menu de opções para o usuário"""
+    while True:
+        print ("\n --- CRUD de Editoras (Biblioteca) ---")
+        print ("1 - Litsar editoras")
+        print ("2 - Adicionar editora")
+        print ("3 - Atualizar editora")
+        print ("4 - Excluir editora")
+        print ("5 - Sair")
+
+        opcao = input ("Escolha uma opção: ")
+
+        if opcao == "1":
+            listar_editoras()
+        elif opcao == "2":
+            #create
+            nome = input ("Digite o nome da editora: ")
+            if nome.strip (): #verifica se o nome não está vazio
+                criar_editora (nome)
+            else:
+                print ("O nome da editora nao pode estar vazio. Por favor, tente novamente.")
+        elif opcao == "3":
+            #update
+            try:
+                id_ed = int (input ("Digite o ID da editora que deseja atualizar: "))
+                novo_nome = input ("Digite o novo nome da editora: ")
+                if novo_nome.strip ():
+                    atualizar_editora (id_ed, novo_nome)
+                else:
+                    print ("O nome da editora nao pode estar vazio. Por favor, tente novamente.")
+            except Exception as e:
+                print ("ID inválido. Por favor, digite um número inteiro.")
+
+        elif opcao == "4":
+            #delete
+            try:
+                id_ed = int (input ("Digite o ID da editora que deseja excluir: "))
+                #confirmação de segurança
+                confirm = input (f"Tem certeza que deseja excluir a editora {id_ed}? (S/N): ").lower()
+                if confirm == "s":
+                    excluir_editora (id_ed)
+                else:
+                    print ("Exclusão cancelada.")
+            except Exception as e:
+                print ("ID inválido. Por favor, digite um número inteiro.")
+        elif opcao == "5":
+            print ("Saindo...")
+            break #quebra o loop while true e encerra o programa
+        else:
+            print ("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == '__main__': #ponto de entrada do script
+    menu()
+
+    
