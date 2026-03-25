142c4fda5d201c3f802d93df5d59432efeb3b4ec
Thalison Santos <135137450+thalisonsantos1@users.noreply.github.com>
2026-03-24 21:46:46 -0300

    go

---
+++ 5 Periodo/Des Sis Web II/2Bim/exemplos/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+	a = 1
+	b := 2.0
+	b = 3.7
+
+	fmt.Println("Hello, World!", a, " ", b)
+}
---
+++ 5 Periodo/Des Sis Web II/2Bim/exercicios/preco_qtd.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var produto string
+	var qtd float64
+	var preco float64
+	var desconto float64
+
+	//produto = "Notebook"
+	qtd = 4
+	preco = 1899.90
+	//qtdTotal = 6
+	desconto = 0.1
+
+	if qtd < 5 {
+		valorTotal := qtd * preco
+		fmt.Println("O valor total é: ", valorTotal)
+	}
+
+	if qtd >= 5 {
+		valorTotal := qtd * preco * (1 - desconto)
+		fmt.Println("O valor total é: ", valorTotal)
+	} 
+
+	
+}
